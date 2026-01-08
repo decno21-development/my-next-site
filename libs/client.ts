3e7a39771b01411228aab24a6825196ec2ef183c
decno21-development <dec.no21@gmail.com>
2026-01-09 04:36:38 +0900

    retry build with env

--- libs/client.ts
+++ libs/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
-  // もし環境変数が空なら、直接文字列を入れてしまう（テスト用）
-  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN || 'd4szsda8hr',
-  apiKey: process.env.MICROCMS_API_KEY || '2wZkbdMZdI2GGBg27I69NFN8g7wASDD5SqWR',
+  // ここで .env.local や Vercel の設定から値を引っ張ってきます
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN || '',
+  apiKey: process.env.MICROCMS_API_KEY || '',
 });
